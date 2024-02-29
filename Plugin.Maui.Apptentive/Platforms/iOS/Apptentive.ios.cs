4b7b08c51119b4201f12877a3888055011ce4b28
Frank Schmitt <frank.schmitt@alchemer.com>
2024-02-28 17:46:01 -0800

    Add most remaining methods into IApptentive and Apptentive.ios.cs

--- Plugin.Maui.Apptentive/Platforms/iOS/Apptentive.ios.cs
+++ Plugin.Maui.Apptentive/Platforms/iOS/Apptentive.ios.cs
@@ -1,4 +1,5 @@
 using ApptentiveKit.iOS;
+using Foundation;
 
 namespace Plugin.Maui.Apptentive;
 
@@ -18,7 +19,95 @@ partial class ApptentiveImplementation: IApptentive
         ApptentiveIOS.Shared.Engage(Event, null);
     }
 
+    public void CanShowInteraction(string Event, Action<bool> completion) {
+        ApptentiveIOS.Shared.QueryCanShowInteraction(Event, (bool result) => completion(result));
+    }
+
     public void PresentMessageCenter() {
         ApptentiveIOS.Shared.PresentMessageCenter(null);
+    }
+
+    public void CanShowMessageCenter(Action<bool> completion) {
+        ApptentiveIOS.Shared.QueryCanShowMessageCenter((bool result) => completion(result));
+    }
+
+    public void SetPersonName(string PersonName) {
+        ApptentiveIOS.Shared.PersonName = PersonName;
+    }
+
+    public void setPersonEmailAddress(string PersonEmailAddress) {
+        ApptentiveIOS.Shared.PersonEmailAddress = PersonEmailAddress;
+    }
+
+    public void addCustomPersonData(string Key, string Value) {
+        ApptentiveIOS.Shared.AddCustomPersonData(Value, Key);
+    }
+
+    public void addCustomPersonData(string Key, double Value) {
+        ApptentiveIOS.Shared.AddCustomPersonData(new NSNumber(Value), Key);
+    }
+
+    // TODO: Add overloads for int, float, etc.?
+
+    public void addCustomPersonData(string Key, bool Value) {
+        ApptentiveIOS.Shared.AddCustomPersonData(Value, Key);
+    }
+
+    public void removeCustomPersonData(string Key) {
+        ApptentiveIOS.Shared.RemoveCustomPersonData(Key);
+    }
+
+    public void addCustomDeviceData(string Key, string Value) {
+        ApptentiveIOS.Shared.AddCustomDeviceData(Value, Key);
+    }
+
+    public void addCustomDeviceData(string Key, double Value) {
+        ApptentiveIOS.Shared.AddCustomDeviceData(new NSNumber(Value), Key);
+    }
+
+    // TODO: Add overloads for int, float, etc.?
+
+    public void addCustomDeviceData(string Key, bool Value) {
+        ApptentiveIOS.Shared.AddCustomDeviceData(Value, Key);
+
+    }
+
+    public void removeCustomDeviceData(string Key) {
+        ApptentiveIOS.Shared.RemoveCustomDeviceData(Key);
+    }
+
+    // TODO: Push notification integration
+    // This should maybe be platform-specific, where they `using` the native module. 
+
+    public int UnreadMessageCount { get {
+        return (int)ApptentiveIOS.Shared.UnreadMessageCount;
+    }}
+
+    public void sentAttachmentText(string Text) {
+        ApptentiveIOS.Shared.SendAttachment(Text);
+    }
+
+    //public void sendAttachmentImage(System.Drawing.Image image) {
+        // FIXME: Not clear how to turn System.Drawing.Image into UIImage. 
+        //ApptentiveIOS.Shared.sendAttachment()
+    //}
+
+    public void sendAttachmentFile(System.IO.Stream file, string MimeType) {
+        ApptentiveIOS.Shared.SendAttachment(NSData.FromStream(file), MimeType);
+    }
+
+    public void LogIn(string Token, Action<bool, string?> Completion) {
+        // FIXME: add this to the API definition
+        //ApptentiveIOS.Shared.LogIn(Token, (bool Success, string Error) => Completion(Success, Error) );
+    }
+
+    public void LogOut() {
+        // FIXME: add this to the API definition
+        //ApptentiveIOS.Shared.LogOut();
+    }
+
+    public void UpdateToken(string Token) {
+        // FIXME: add this to the API definition
+        //ApptentiveIOS.Shared.UpdateToken();
     }
 }
