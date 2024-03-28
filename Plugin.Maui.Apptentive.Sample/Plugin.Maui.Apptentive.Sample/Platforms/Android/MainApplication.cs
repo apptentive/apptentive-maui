9cebc4b57c103bc8b90022d3528a64716d4699d8
Frank Schmitt <frank.schmitt@alchemer.com>
2024-03-28 12:49:32 -0700

    WIP

--- Plugin.Maui.Apptentive.Sample/Plugin.Maui.Apptentive.Sample/Platforms/Android/MainApplication.cs
+++ Plugin.Maui.Apptentive.Sample/Plugin.Maui.Apptentive.Sample/Platforms/Android/MainApplication.cs
@@ -1,5 +1,9 @@
 ﻿using Android.App;
 using Android.Runtime;
+using ApptentiveSDK;
+using ApptentiveCore;
+using ApptentiveLogLevel = Apptentive.Com.Android.Util.LogLevel;
+
 
 namespace Plugin.Maui.Apptentive.Sample;
 
@@ -9,8 +13,8 @@ public class MainApplication : MauiApplication
 	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
 		: base(handle, ownership)
 	{
-		 var configuration = new ApptentiveConfiguration("ANDROID-XOLOFAX-cdfb1ae00eeb", "2c1883d1d22686f4588ad9126f037cd7");
-            configuration.LogLevel = ApptentiveLogLevel.Verbose;
+		 var configuration = new ApptentiveSDK.ApptentiveConfiguration("ANDROID-XOLOFAX-cdfb1ae00eeb", "2c1883d1d22686f4588ad9126f037cd7");
+            //configuration.logLevel = ApptentiveLogLevel.Verbose;
             ApptentiveSDK.Apptentive.Register(this,configuration);
             Android.Util.Log.Info("XoloFax", "Apptentive Registered!");
 	}
