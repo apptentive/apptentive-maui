9c7aabb38ef31d0a2eb1300bf366b9baaf7921e7
Frank Schmitt <frank.schmitt@alchemer.com>
2024-03-28 14:27:14 -0700

    Seems to work

--- Plugin.Maui.Apptentive.Sample/Plugin.Maui.Apptentive.Sample/Platforms/Android/MainActivity.cs
+++ Plugin.Maui.Apptentive.Sample/Plugin.Maui.Apptentive.Sample/Platforms/Android/MainActivity.cs
@@ -2,13 +2,17 @@
 using Android.Content.PM;
 using Android.OS;
 using ApptentiveSDK;
-using ApptentiveCore;
 
 namespace Plugin.Maui.Apptentive.Sample;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
-     ApptentiveSDK.Apptentive.Engage("test");
+     protected override void OnCreate(Bundle savedInstanceState)
+     {
+          base.OnCreate(savedInstanceState);
+
+          ApptentiveSDK.Apptentive.Engage("test");
+     }
 }
 
