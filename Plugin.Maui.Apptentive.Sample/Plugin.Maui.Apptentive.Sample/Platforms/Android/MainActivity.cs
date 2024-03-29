db6804717db14bbff2eb473acc1ff62a2bf8b319
Luqmaan <luq.j.khan@gmail.com>
2024-03-29 13:04:12 -0500

    register main activity

--- Plugin.Maui.Apptentive.Sample/Plugin.Maui.Apptentive.Sample/Platforms/Android/MainActivity.cs
+++ Plugin.Maui.Apptentive.Sample/Plugin.Maui.Apptentive.Sample/Platforms/Android/MainActivity.cs
@@ -6,13 +6,23 @@ using ApptentiveSDK;
 namespace Plugin.Maui.Apptentive.Sample;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
-public class MainActivity : MauiAppCompatActivity
+public class MainActivity : MauiAppCompatActivity, IApptentiveActivityInfo
 {
      protected override void OnCreate(Bundle savedInstanceState)
      {
           base.OnCreate(savedInstanceState);
-
-          ApptentiveSDK.Apptentive.Engage("hey");
+           ApptentiveSDK.Apptentive.RegisterApptentiveActivityInfoCallback(this);
      }
+
+
+         public Activity ApptentiveActivityInfo {
+            get {
+                return this;
+            }
+        }
+
+         public Activity GetApptentiveActivityInfo() {
+            return this;
+        }
 }
 
